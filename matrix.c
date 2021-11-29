b2faf239c78d1de4a14bbfcf6a1bea2c5bfd6f4b
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2021-11-29 18:42:09 +0530

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,m,n,a[10][10];
+
+    printf("Enter the no of rows and coloumns in the matrix");
+    scanf("%d%d",&m,&n);
+
+    printf("Enter the elements of matrix rowwise");
+
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}

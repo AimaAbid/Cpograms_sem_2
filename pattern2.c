84071330352fa4cbcb0e1d8b5a52f86d22c77d91
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2021-11-29 14:33:12 +0530

    Add files via upload

---
+++ pattern2.c
@@ -0,0 +1,17 @@
+// invetred pyramid of *
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<7;i++)
+    {
+        for(j=1;j<8-i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}

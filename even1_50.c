36f353249847f8f75e16243c0827955a84e800de
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2021-11-29 11:20:36 +0530

    Add files via upload

---
+++ even1_50.c
@@ -0,0 +1,19 @@
+//even nos from 1to 50
+#include<stdio.h>
+int main()
+{
+    int i;
+
+    for(i=1;i<51;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d is even\n",i);
+        }
+
+    }
+
+    return 0;
+
+
+}

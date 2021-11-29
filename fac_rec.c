e1fad32af6495a46fc3648abdcd9afab933ebd07
Aima Abid <95177608+AimaAbid@users.noreply.github.com>
2021-11-29 20:23:45 +0530

    Add files via upload

---
+++ fac_rec.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fac(int n)
+{
+    int a;
+    if(n>1)
+    {
+        a=n*fac(n-1);
+        return a;
+    }
+
+    else 
+    return 1;
+}
+int main()
+{
+    int i,num;
+    printf("enter the no");
+    scanf("%d",&num);
+    printf("the factorial is %d",fac(num));
+    return 0;
+
+}
